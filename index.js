7c01a3e908f19c75a0fcb8ac1379e1c6940d1e79
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-11-25 01:07:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let text = document.getElementById("text")
+  let text = document.getElementById("text");
+  l.textContent = "This is really cool!"
 })
